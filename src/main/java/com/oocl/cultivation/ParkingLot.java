3732c88f796085fc02e6f0c4f542c80240bb68a9
kenckl <klcheung8355@gmail.com>
2020-11-27 15:18:58 +0800

    [Parking][Ken] Implemented function for return ticket when parking given car

--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -8,6 +8,7 @@ public class ParkingLot {
     private final Map<Ticket, Car> ticketCarMap = new HashMap<>();
 
     public Ticket park(Car car){
-        return null;
+        Ticket ticket = new Ticket();
+        return ticket;
     }
 }
