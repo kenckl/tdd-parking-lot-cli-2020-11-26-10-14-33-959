0c4e827a62c75cc6ff27d3529bedee7b84be1ef5
kenckl <klcheung8355@gmail.com>
2020-11-27 15:17:04 +0800

    [Parking][Ken] Implemented test case for return ticket when parking given car

---
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -0,0 +1,13 @@
+package com.oocl.cultivation;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ParkingLot {
+
+    private final Map<Ticket, Car> ticketCarMap = new HashMap<>();
+
+    public Ticket park(Car car){
+        return null;
+    }
+}
--- src/test/java/com/oocl/cultivation/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/ParkingBoyTest.java
@@ -1,10 +1,20 @@
 package com.oocl.cultivation;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ParkingBoyTest {
     @Test
-    void write_your_first_test() {
+    void should_return_parking_ticket_when_parking_given_parking_boy_park() {
+        //GIVEN
+        Car car = new Car();
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
 
+        //WHEN
+        Ticket ticket = parkingBoy.park(car);
+
+        //THEN
+        assertNotNull(ticket);
     }
 }
