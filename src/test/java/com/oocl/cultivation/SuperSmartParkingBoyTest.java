8b98ddf30ac3e2e0ca24c8ab250b584683df26f8
kenckl <klcheung8355@gmail.com>
2020-11-29 09:44:50 +0800

    [Parking][Ken] Implemented test case for SuperSmartParkingBoy for returning a car when fetching given a valid parking ticket

--- src/test/java/com/oocl/cultivation/SuperSmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/SuperSmartParkingBoyTest.java
@@ -19,11 +19,29 @@ public class SuperSmartParkingBoyTest {
         List<ParkingLot> parkingLot = new ArrayList<>();
         parkingLot.add(new ParkingLot());
         SuperSmartParkingBoy superSmartParkingBoy = new SuperSmartParkingBoy();
-        
+
         //WHEN
         Ticket ticket = superSmartParkingBoy.parkCar(car);
 
         //THEN
         assertNotNull(ticket);
     }
+
+    @Test
+    public void should_return_a_car_when_fetching_given_a_valid_parking_ticket() {
+        //given
+        Car car = new Car();
+        List<ParkingLot> parkingLot = new ArrayList<>();
+        SuperSmartParkingBoy superSmartParkingBoy= new SuperSmartParkingBoy(parkingLot);
+        parkingLot.add(new ParkingLot());
+        Ticket ticket = superSmartParkingBoy.parkCar(car);
+
+        //when
+        Car fetchedCar = superSmartParkingBoy.fetchCar(ticket);
+
+        //then
+        assertSame(car, fetchedCar);
+    }
+
+
 }
